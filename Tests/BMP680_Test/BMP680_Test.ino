0d013065b26f7dedc2ee13ddc57c31211e1031c9
BinaryMongoose <96893291+BinaryMongoose@users.noreply.github.com>
2023-02-13 19:07:21 -0700

    Created BME680 Test.
    Just seeing how to program a BME680.

---
+++ Tests/BMP680_Test/BMP680_Test.ino
@@ -0,0 +1,48 @@
+#include <Adafruit_GPS.h>
+#include <Adafruit_PMTK.h>
+#include <NMEA_data.h>
+#include <Adafruit_BME680.h>
+#include <bme68x.h>
+#include <bme68x_defs.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME680 bme; 
+
+void setup() {
+  Serial.begin(9600);  // Setting up Serial
+  Serial.println(F("BME680 test"));
+
+  if (!bme.begin()) {
+    Serial.println("Could not find the BME sensor!");
+  }
+
+  // Set up oversampling and filter initialization.
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150);  // 320*C for 150 ms
+}
+
+void loop() {
+  if (!bme.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+  
+  Serial.print("Temperature = ");
+  Serial.print(bme.temperature);
+  Serial.println(" *C");
+
+  Serial.print("Gas = ");
+  Serial.print(bme.gas_resistance / 1000.0);
+  Serial.println(" KOhms");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.println();
+  delay(2000);
+}
